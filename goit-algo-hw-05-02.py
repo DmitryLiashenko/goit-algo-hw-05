b5275b2352f3904dcd9d7a7b1547a90ef0d4be2b
DmitryLiashenko <dmliash1@gmail.com>
2024-02-11 14:36:39 +0200

    Commit-hw-05-02-welldone

--- goit-algo-hw-05-02.py
+++ goit-algo-hw-05-02.py
@@ -1,17 +1,26 @@
+import re
 from typing import Callable
+from functools import reduce
 
 
 def generator_numbers(text: str):
-    pass
-
-    yield
+    """
+    Генератор, извлекающий числа с плавающей точкой из текста.
+    """
+    pattern = r"\d+[.]\d+"                # ИСПОЛЬЗУЕМ raw-СТРОКУ КАК ПАТЕРН
+    numbers = re.findall(pattern, text)   # СОЗДАЕМ СПИСОК ИЗ НАЙДЕНОЙ ИНФОРМАЦИИ СОГЛАСНО ПАТЕРНУ
+    for number in numbers:                # ИТЕРИРУЕМ СПИСОК 
+        yield float(number)               # ВОЗВРАЩАЕМ В ФУНКЦИЮ ЧИСЛО(float) И ЗАПОМИНЕМ СОСТОЯНИЕ ФУНКЦИИ
 
 
 def sum_profit(text: str, func: Callable):
-    pass 
+    """
+    Функция, вычисляющая сумму чисел, извлеченных из текста с помощью переданной функции.
+    """
+    return reduce(lambda x, y: x + y, func(text)) # Используем reduce для суммирования результатов функции
 
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01\
         як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
-total_income = sum_profit(text, generator_numbers)
-print(f"Загальний дохід: {total_income}")
+
+print(f"Загальний дохід: {sum_profit(text, generator_numbers)}")
